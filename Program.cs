ba5929f3ac0242880bd2d2cd0f7db65c6372ea44
Ryan <zelda0169@gmailcom>
2020-10-02 16:16:35 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace IOd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please type in some text");
+            string text = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\Ryan\Desktop\Coding", text);
+            string textr = File.ReadAllText(@"C:\Users\Ryan\Desktop\Coding\ some.txt");
+            Console.WriteLine(textr);
+            Console.ReadLine();
+        }
+    }
+}
